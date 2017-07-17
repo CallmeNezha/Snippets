762e462c1a7bd1d7b05e05102cc7de89a396baf8
Nezha <783627014@qq.com>
2017-07-18 02:30:59 +0800

    Modify: socket

--- Python/Socket Programming/server.py
+++
@@ -1,8 +0,0 @@
-import socket_server
-
-if "__main__" == __name__:
-    with socket_server.EnvServer() as env_server:
-        data = env_server.recv_data()
-        if data is not None:
-            print(data.decode('ascii'))
-    print("[*] Program done.")
--- Python/Socket Programming/socket_server.py
+++ Python/Socket Programming/socket_server.py
@@ -1,91 +1,33 @@
-import socket
+import socketserver
 import time
 
-Code = {
-    b"4509": "transfer_done_continue",
-    b"4510": "transfer_done_switch",
-}
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
 
-class EnvServer:
-    def __init__(self, host=None, port=None):
-        self.host = "127.0.0.1" if host is None else host
-        self.port = 12345 if port is None else port
-        self.state = "closed"
-    
-    def __enter__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("[*] Listening on: {0}: {1}".format(self.host, self.port))
-        self.sock.bind((self.host, self.port))
-        self.sock.listen(1) # Only accept one connection
-        print("[*] Waiting for connection...")
-        self.conn, addr = self.sock.accept()
-        print('[*] Got connection from', str(addr))
-        self.state = "connected"
-        return self
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.sock.close()
-        print("[*] Connection closed.")
+    def handle(self):
+        for i in range(3):
+            # self.request is the TCP socket connected to the client
+            self.data = self.request.recv(1024).strip()
+            print("[*]{0} wrote: {1}".format(self.client_address[0], self.data))
 
-    def recv_data(self):
-        if self.state != "connected": return None
-        while True:
-            data, code = self._recv_data(self.conn)
-            if b'2' == code:
-                break
-            if b'1' == code:
-                break
-            if b"4509" == code:
-                return data.decode('ascii')
-            if b"4510" == code:
-                return data.decode('ascii')
+            time.sleep(10) #wait for 10 seconds as like tensorflow computing time
+            # just send back the same data, but upper-cased
+            self.request.sendall(self.data.upper())
+            print("[*]Send: {0}".format(self.data.upper()))
 
-        print("[*] Ready to close connection.")
-        self.conn.close()
-        self.state = "closed"
-        return None
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
 
-    def send_data(self, data_string):
-        if self.state != "connected": return
-        self.conn.sendall(data_string.encode('ascii'))
-        
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
-    def _recv_data(self, conn):
-        data = []
-        code = b'0'
-        while True:
-            try:
-                data_chunk = conn.recv(1024)
-            except socket.error as error:
-                print("[!!!] Socket error", str(error))
-                code = b'1'
-                break
-            if not data_chunk:
-                code = b'2'
-                break
-            if data_chunk in Code:
-                code = data_chunk
-                break
-            data.append(data_chunk)
-        return b''.join(data), code
-
-
-if "__main__" == __name__:
-    with EnvServer() as env_server:
-        while True:
-            # data = env_server.recv_data()
-            # if data is not None:
-            #     print(data.decode('ascii'))
-            env_server.send_data("COMMAND=RESET")
-            print("[*] Send msg:", "COMMAND=RESET")
-            time.sleep(2)
-            data = env_server.recv_data()
-            print("[*] Recv msg:", data)
-            time.sleep(2)
-            env_server.send_data("COMMAND=STEP ACTION=[1.0,23.0,2.0]")
-            print("[*] Send msg:", "COMMAND=STEP ACTION=[1.0,23.0,2.0]")
-            time.sleep(2)
-            print("[*] Recv msg:", data)
-            time.sleep(2)
-            
-    print("[*] Program done.")
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
+    server.server_close()
