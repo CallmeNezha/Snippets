7be24efa7e17e44e32f38201c2db701312d63391
xhm <525150210@qq.com>
2019-03-20 02:12:55 +0800

    SimpleServer支持Python2.7 调整文件结构

--- Python/Simple Server/simple_server.py
+++ Python/Simple Server/simple_server.py
@@ -1,12 +1,20 @@
 import sys
-import locale
-import http.server
-import socketserver
 
-addr = len(sys.argv) < 2 and "localhost" or sys.argv[1]
-port = len(sys.argv) < 3 and 8000 or locale.atoi(sys.argv[2])
+version = sys.version_info[:2]
 
-handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer((addr, port), handler)
-print ("HTTP server is at: http://%s:%d/" % (addr, port))
-httpd.serve_forever()
+PORT = 8000
+
+if version <= (2, 7):
+    import SimpleHTTPServer
+    import SocketServer
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("127.0.0.1", PORT), Handler)
+    print("serving at %s port %d" % httpd.server_address[:2])
+    httpd.serve_forever()
+else:
+    import http.server
+    import socketserver
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
+        print("serving at %s port %d" % httpd.server_address[:2])
+        httpd.serve_forever()
